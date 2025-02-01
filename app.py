1d228ef79d32c492a8a21325778f0da0a537af44
azharhasan <101918996+azharhasan@users.noreply.github.com>
2025-02-02 02:03:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import gradio as gr
+from fastai.vision.all import *
+from fastbook import *
+import pathlib
 
-def greet(name):
-    return "Hello " + name + "!!"
+plt = platform.system()
+if plt != 'Windows': pathlib.WindowsPath = pathlib.PosixPath
+
+ln = load_learner("model.pkl")
+catagories = ("Dog", "Cat")
+
+def classify_image(img):
+    pred, idx, probs = ln.predict(img)
+    return dict(zip(catagories, map(float, probs)))
+
+intf = gr.Interface(fn=classify_image, inputs=gr.Image(), outputs=gr.Label())
+intf.launch(inline=False)
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 demo.launch()
